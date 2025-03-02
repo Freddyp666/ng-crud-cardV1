fae029fda0d05eec59a8ed7e086e7019584fea99
Freddyp666 <pat-.-freddy@outlook.com>
2025-03-02 15:15:50 -0500

    nuevas rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from "./components/card/card.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CardComponent, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'  
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import path from 'path';
 
 
 export const routes: Routes = [
+    {path:'', redirectTo: '/auth/login', pathMatch: 'full'},
+    {path: 'auth', component: AuthComponent
+    , children: [
+        {path: 'login', component: LoginComponent},
+        {path: 'register', component: RegisterComponent}
+
+    ]
+    },
     {path: '', component: CardComponent},
-    {path: 'auth', component: AuthComponent}
 ];
