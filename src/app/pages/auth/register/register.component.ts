86108bd83e5e5f4fb50f114d50a998eab60627de
Freddyp666 <pat-.-freddy@outlook.com>
2025-03-06 01:53:15 -0500

    Solucion de Rutas

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.sass'
 })
